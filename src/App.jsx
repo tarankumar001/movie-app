ff0a42347a25c2c75e6b44a299872b93cbec3765
Taran Kumar <140129581+tarankumar001@users.noreply.github.com>
2025-09-13 15:59:32 +0530

    initial

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const App = () => {
+  return (
+    <div>
+          <h1 class="text-3xl font-bold underline">    Hello world!  </h1>
+ 
+    </div>
+  )
+}
+
+export default App
